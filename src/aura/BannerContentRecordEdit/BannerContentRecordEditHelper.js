c645826122360810a0c0fa41e4e050a5cb203393
Matias Novello <matias.novello@cloudgaia.com>
2019-04-29 17:20:05 -0300

    Fix choose layout on banner, advance content and block

--- src/aura/BannerContentRecordEdit/BannerContentRecordEditHelper.js
+++ src/aura/BannerContentRecordEdit/BannerContentRecordEditHelper.js
@@ -22,7 +22,7 @@
 						component.set("v.contentData.BannerFrameType",data.bannerFrameTypes[0]);                        
                     }
 
-                    if(data.content.MediaElementAssignments != null){
+                    if(data.content.MediaElementAssignments != null && data.content.MediaElementAssignments.length > 0){
 						component.set('v.imageUrl', data.content.MediaElementAssignments[0].MediaElement.FileURLDesktop);
                         component.set('v.mediaElementName', data.content.MediaElementAssignments[0].MediaElement.Name);
                         component.set('v.mediaElementId', data.content.MediaElementAssignments[0].MediaElement.Id);

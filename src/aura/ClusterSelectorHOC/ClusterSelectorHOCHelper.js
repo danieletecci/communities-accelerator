003fdfaf71c769de47909c80a10b37c438065909
Iris Espinoza <iristheeight@gmail.com>
2019-04-28 21:14:55 -0300

    Global methods to Diageo

--- src/aura/ClusterSelectorHOC/ClusterSelectorHOCHelper.js
+++ src/aura/ClusterSelectorHOC/ClusterSelectorHOCHelper.js
@@ -1,6 +1,6 @@
 ({
     getComponentWrapper: function (component, helper) {
-    	var cw = component.get("v.componentWrapper");
+        var cw = component.get("v.componentWrapper");
         var device = $A.get("$Browser.formFactor");
 
         var action = component.get("c.getComponentWrapper");        
@@ -52,11 +52,25 @@
 
         $A.enqueueAction(action);
     },
-
     setCookie : function(cname, cvalue, exdays) {
         var d = new Date();
         d.setTime(d.getTime() + (exdays*24*60*60*1000));
         var expires = "expires="+ d.toUTCString();
         document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    }  
+    },
+    getCookie : function(cname){
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    },
 })

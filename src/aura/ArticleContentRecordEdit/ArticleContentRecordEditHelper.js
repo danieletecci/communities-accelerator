e07090a9e8605cbe3b2e3b2e8cb3c753ddc82192
Daniele Tecci <daniele.tecci@cloudgaia.com>
2019-05-09 09:16:30 -0300

    Article Detail fields validation

--- src/aura/ArticleContentRecordEdit/ArticleContentRecordEditHelper.js
+++ src/aura/ArticleContentRecordEdit/ArticleContentRecordEditHelper.js
@@ -114,6 +114,16 @@
 		});
 		toastEvent.fire();
 	},
+	showToast : function(toastTitle,toastMessage,toastType){
+	    var toastEvent = $A.get("e.force:showToast");
+	    toastEvent.setParams({
+	        title: toastTitle,
+	        message: toastMessage,
+	        type: toastType,
+			mode: (toastType === "success") ? 'dismissable' : 'sticky'
+	    });
+	    toastEvent.fire();
+	},
 	stringFormat: function(string) {
 	    var outerArguments = arguments;
 	    return string.replace(/\{(\d+)\}/g, function() {
@@ -125,4 +135,50 @@
 		component.set('v.imageUrl', null);
 		component.set('v.mediaElementName', null);
 	},
+	validateRequiredFields : function(component, callback){
+		var fields = this.getEmptyRequiredFieldsList(component);
+		var errorMessage = $A.get("$Label.c.ReviewFields") + "\n";
+		if(!$A.util.isEmpty(fields)){
+			fields.forEach(function myFunction(item, index, arr) {
+				errorMessage += " • " + item.label + "\n";
+				if(item.isStandard){
+					item.input.reportValidity();
+				} else {
+					if(item.div && !$A.util.hasClass(item.div, "has-errror")){
+						$A.util.addClass(item.div, "has-errror");
+					}
+					if(item.error && !$A.util.hasClass(item.error, "slds-show")){
+						$A.util.addClass(item.error, "slds-show");
+						$A.util.removeClass(item.error, "slds-hide");
+					}
+				}
+			});
+			this.showToast($A.get("$Label.c.MissingRequiredFields"), errorMessage, "error");
+		} else {
+			callback();
+		}
+	},
+	getEmptyRequiredFieldsList : function(component){
+		var fields = [];
+		var content = component.get("v.contentData");
+		
+		this.addField(component.get("v.titleRequired"), fields, $A.get("$Label.c.ArticleContentDetailTitle"), true, component.find("title"), null, null, content.Title);
+		this.addField(component.get("v.extractRequired"), fields, $A.get("$Label.c.ArticleContentDetailExtract"), true, component.find("extract"), null, null, content.Extract);
+		this.addField(component.get("v.bodyRequired"), fields, $A.get("$Label.c.ArticleContentDetailBody"), false, null, null, component.find("bodyError"), content.Body);
+		this.addField(component.get("v.featureRequired"), fields, $A.get("$Label.c.ArticleContentDetailImage"), false, null, component.find("featureDiv"), component.find("featureError"), component.get("v.imageUrl"));
+		this.addField(component.get("v.layoutRequired"), fields, $A.get("$Label.c.ArticleContentDetailChooseLayout"), false, null, component.find("layoutDiv"), component.find("layoutError"), content.Layout);
+
+		return fields;
+	},
+	addField : function(isRequired, fields, label, isStandard, input, div, error, value){
+		if(isRequired && $A.util.isEmpty(value)){
+			fields.push({
+				"isStandard"	: isStandard,
+				"label"			: label,
+				"input"			: input,
+				"div"			: div,
+				"error"			: error
+			});
+		}
+	}
 })
--- src/aura/ContentPublishModal/ContentModalPublishHelper.js
+++ src/aura/ContentPublishModal/ContentModalPublishHelper.js
@@ -43,6 +43,8 @@
 				"publishEndDate"	: component.get("v.endDate")
 			});
 	        cmpEvent.fire();
+			$A.util.removeClass(component.find("publishModal"), "slds-fade-in-open");
+			$A.util.removeClass(component.find("publishModalBackdrop"), "slds-backdrop_open");
 	    } else
 	    	helper.scheduleContent(component);
 	},

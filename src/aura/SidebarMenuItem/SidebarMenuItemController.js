043529bff0102526969517c2a9b37467fbe0a293
aguchez <agustin.sanchez@cloudgaia.com>
2019-05-10 09:53:54 -0300

    sidebar active menu fix

--- src/aura/SidebarMenuItem/SidebarMenuItemController.js
+++ src/aura/SidebarMenuItem/SidebarMenuItemController.js
@@ -7,5 +7,12 @@
 	},
 	redirect : function(component, event, helper) {
         helper.redirect(component);
-	}
+	},
+    urlChange : function(component, event, helper){
+        if('/s/' + component.get("v.item.menu.NavigationURL") === window.location.pathname){
+            component.set("v.isActive", true);
+        }else{
+            component.set("v.isActive", false);
+        }
+    }
 })

6597435b388ea87c77bfed108e08a30774ababb8
sbelaustegui <sebasbelaustegui@hotmail.com>
2019-05-17 17:18:23 -0300

    mobile bug fix

--- src/lwc/cardList/cardList.js
+++ src/lwc/cardList/cardList.js
@@ -18,41 +18,40 @@ export default class CardList extends LightningElement {
     @api allContentsQuantity;
 
     viewMoreLabel;
+    mobile;
 
     constructor(){
         super();
         this.viewMoreLabel = ViewMoreLabel;
+        this.mobile = navigator.userAgent.toLowerCase().includes('mobi');
     }
     connectedCallback() {
-        this.valie = ViewMoreLabel;
         loadStyle(this, 'sfsites/c/resource/Assets/Assets/Styles/cardListExternalStyles.css');
     }
 
     get containerClass() {
-        let mobile = navigator.userAgent.toLowerCase().includes('mobi');
         let iscarrousel = this.isCarrousel ? ' carrousel': '';
-        return mobile ?  'list-container' + iscarrousel : 'list-container slds-grid slds-wrap' + iscarrousel;
+        return this.mobile ?  'list-container' + iscarrousel : 'list-container slds-grid slds-wrap' + iscarrousel;
     }
     get cardWrapperClass() {
-        let mobile = navigator.userAgent.toLowerCase().includes('mobi');
         if(this.formfactor === 'TABLET')
             return 'slds-col slds-size_2-of-4';
-        return mobile ? 'slds-col slds-size_4-of-4' : 'slds-col slds-size_1-of-' + this.numberofcolumns;
+        return this.mobile ? 'slds-col slds-size_4-of-4' : 'slds-col slds-size_1-of-' + this.numberofcolumns;
     }
     
     get isCarrousel() {
-        let iscarousel = this.pagingtype === 'Carousel' & this.showpaging;
+        let iscarousel = this.pagingtype === 'Carousel' && this.showpaging && !this.mobile ;
         return iscarousel;
     }
 
     get isViewMoreAndHasHidedElements() {
-        let isViewMore = this.pagingtype === 'View More' & this.showpaging;
+        let isViewMore = (this.pagingtype === 'View More' && this.showpaging) || this.mobile;
         let hasHiddenElements = this.contents.length < this.allContentsQuantity;
         return isViewMore && hasHiddenElements;
     }
     
     get isPagingBottom() {
-        let ispagingbottom = this.pagingtype === 'Paging Bottom' & this.showpaging;
+        let ispagingbottom = this.pagingtype === 'Paging Bottom' && this.showpaging && !this.mobile;
         return ispagingbottom;
     }
 

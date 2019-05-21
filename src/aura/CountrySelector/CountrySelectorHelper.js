39dffbf8c09bdf0a6910f1421efa93ddd071fbc0
aguchez <agustin.sanchez@cloudgaia.com>
2019-05-21 18:03:43 -0300

    phone country selector default value

--- src/aura/CountrySelector/CountrySelectorHelper.js
+++ src/aura/CountrySelector/CountrySelectorHelper.js
@@ -2,7 +2,9 @@
     getcountriesCode : function(component, event, helper) {
 
     	var action = component.get("c.getCountriesCodes");
-
+		action.setParams({
+                defaultValue: component.get('v.defaultValue')
+            });
         action.setCallback(this, function(f){
             if(f.getState() === "SUCCESS") {
                 component.set("v.CountriesWrapper", action.getReturnValue());
